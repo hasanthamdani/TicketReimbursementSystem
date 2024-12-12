2b3fc6b08ae954f494aecbd3c58996037d216743
hasanthamdani <hasanthamdani01@gmail.com>
2024-12-12 13:43:51 -0500

    Spring Initializr project with build in depenedencies

---
+++ src/main/java/com/Revature/ticketreimbursement/TicketreimbursementApplication.java
@@ -0,0 +1,13 @@
+package com.Revature.ticketreimbursement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketreimbursementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketreimbursementApplication.class, args);
+	}
+
+}
